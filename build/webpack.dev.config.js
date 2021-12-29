258616de9f8ff567b548a63936bb0beeb85d17d8
wuguang <wuguang_123@163.com>
2021-12-30 07:00:20 +0800

    update

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -5,9 +5,12 @@ const path = require('path');
 module.exports= merge(baseConfig,{
     mode:'development',
     devServer:{
+        publicPath:"../dist",
+        /*
         static:[{
             directory: path.join(__dirname, '../dist'),
         }],
+        */
         compress:true,
         port:8080,
         //historyApiFallback: true,
