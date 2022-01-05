3f1ef5d959f3307060c56449a517b9a2f80c1124
wuguang <wuguang@wxblockchain.com>
2022-01-05 18:57:39 +0800

    add split chunk

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -46,6 +46,28 @@ let prodConfig = merge(baseConfig,{
 		}),
 	],
 	optimization: {
+		splitChunks: {
+			chunks: 'async', // 有效值为 `all`，`async` 和 `initial`
+			minSize: 20000, // 生成 chunk 的最小体积（≈ 20kb)
+			minRemainingSize: 0, // 确保拆分后剩余的最小 chunk 体积超过限制来避免大小为零的模块
+			minChunks: 1, // 拆分前必须共享模块的最小 chunks 数。
+			maxAsyncRequests: 30, // 最大的按需(异步)加载次数
+			maxInitialRequests: 30, // 打包后的入口文件加载时，还能同时加载js文件的数量（包括入口文件）
+			enforceSizeThreshold: 50000,
+			cacheGroups: { // 配置提取模块的方案
+			  defaultVendors: {
+				test: /[\/]node_modules[\/]/,
+				priority: -10,
+				reuseExistingChunk: true,
+			  },
+			  default: {
+				minChunks: 2,
+				priority: -20,
+				reuseExistingChunk: true,
+			  },
+			},
+		},
+	
 		minimize: true,
 		minimizer: [
 			//压缩css
